d3abf6b4ec1a8f78b2156ed87fb2078d70cfa419
Ilvictory2151 <58403652+Ilvictory2151@users.noreply.github.com>
2020-02-05 21:07:11 -0800

    Add files via upload

---
+++ Hello/AppProgression1.py
@@ -0,0 +1,23 @@
+print("Hello World")
+print("My name is Victory T. Tamang.")
+print("Testing")
+
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+age = ("14")
+print("My name is Victory T. Tamang and I'm " + age + "-years-old.")
+
+name = ("Victory T. Tamang")
+print("I'm " + name + " and I live in California")
+
+location = ("California")
+year = ("2005")
+print("I'm " + name + " and was born in " + year + ". I live currently live in " + location + ".")
+
+print(name.upper())
+print(name.lower())
+
+print(name.isupper())
